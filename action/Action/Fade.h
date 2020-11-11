d1558b3f5f4766f63f03def15b28b0969317cdbf
ono-hikaru81 <Onohikaru0801@gmail.com>
2020-11-11 09:36:13 +0900

    フェード処理を実装。 - #1

---
+++ action/Action/Fade.h
@@ -0,0 +1,29 @@
+#ifndef FADE_H
+#define FADE_H
+
+//==============================
+//フェードクラス
+//==============================
+class Fade
+{
+public:
+	int m_alpha;
+	bool m_isFading;
+	int m_fadeType;
+
+public:
+	Fade();
+	virtual~Fade();
+	void Start(enum FadeType fadeType_);			// フェードの種類を決める
+	void Draw();							// 矩形を描画する関数
+	void Exec(float fadeSpeed_);			// フェード処理を実行
+	bool IsFading();						// フェードが終わったかどうか
+};
+
+enum FadeType
+{
+	fadeIn,
+	fadeOut
+};
+
+#endif // !FADE_H
